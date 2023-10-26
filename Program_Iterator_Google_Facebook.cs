20d6a8b1e7b351e393137cac57e9420e6e1ea0d2
Waldhemir S. Rios Miranda <wwaldyy@gmail.com>
2023-10-26 18:41:25 -0400

    Add Program work
    Adicionando soluciones al proyecto,
    Sin utilizar el padrón Iterator
    utilizando Iterator pero trabajando solo con users de google
    utilizando Iterator pero trabajando con users de google y Facebook

---
+++ Program_Iterator_Google_Facebook.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    public interface IUser
+    {
+        string GetName();
+        string GetInfo();
+    }
+
+    public class GoogleUser : IUser
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public GoogleUser(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetInfo()
+        {
+            return $"Name: {Name}, Email: {Email}";
+        }
+    }
+
+    public class FacebookUser : IUser
+    {
+        public string FbName { get; set; }
+        public string FbUrl { get; set; }
+
+        public FacebookUser(string fbName, string fbUrl)
+        {
+            FbName = fbName;
+            FbUrl = fbUrl;
+        }
+
+        public string GetName()
+        {
+            return FbName;
+        }
+
+        public string GetInfo()
+        {
+            return $"Name: {FbName}, URL: {FbUrl}";
+        }
+    }
+
+    // Definimos una interfaz Iterator genérica.
+    public interface IIterator<T>
+    {
+        T Current { get; }
+        bool HasNext();
+        void Next();
+    }
+
+    // Definimos una interfaz Iterable genérica.
+    public interface IEnumerableCollection<T>
+    {
+        IIterator<T> GetIterator();
+    }
+
+    public class UserIterator<T> : IIterator<T> where T : IUser
+    {
+        private List<T> users;
+        private int index;
+
+        public UserIterator(List<T> users)
+        {
+            this.users = users;
+            this.index = 0;
+        }
+
+        public T Current
+        {
+            get { return users[index]; }
+        }
+
+        public bool HasNext()
+        {
+            return index < users.Count;
+        }
+
+        public void Next()
+        {
+            index++;
+        }
+    }
+
+    public class ShowUsersPanel
+    {
+        public void ShowUsers<T>(IEnumerableCollection<T> api) where T : IUser
+        {
+            var iterator = api.GetIterator();
+
+            while (iterator.HasNext())
+            {
+                Console.WriteLine(iterator.Current.GetInfo());
+                iterator.Next();
+            }
+        }
+    }
+
+    //Generamos el GoogleApi
+    public class GoogleApi : IEnumerableCollection<GoogleUser>
+    {
+        private readonly List<GoogleUser> googleUsers = new List<GoogleUser>();
+
+        public GoogleApi()
+        {
+            googleUsers.Add(new GoogleUser("roberto", "r@gmail.com"));
+            googleUsers.Add(new GoogleUser("luis", "l@gmail.com"));
+            googleUsers.Add(new GoogleUser("ana", "a@gmail.com"));
+        }
+
+        public IIterator<GoogleUser> GetIterator()
+        {
+            return new UserIterator<GoogleUser>(googleUsers);
+        }
+    }
+
+
+    //Generamos el FacebookApi
+    public class FacebookApi : IEnumerableCollection<FacebookUser>
+    {
+        public List<FacebookUser> GetFacebookUsers()
+        {
+            return new List<FacebookUser>
+            {
+                new FacebookUser("juan", "link1"),
+                new FacebookUser("carlos", "link2")
+            };
+        }
+
+        public IIterator<FacebookUser> GetIterator()
+        {
+            return new UserIterator<FacebookUser>(GetFacebookUsers());
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var showUsersPanel = new ShowUsersPanel();
+
+            Console.WriteLine("Google Users:");
+            var googleApi = new GoogleApi();
+            showUsersPanel.ShowUsers(googleApi);
+
+            Console.WriteLine("Facebook Users:");
+            var facebookApi = new FacebookApi();
+            showUsersPanel.ShowUsers(facebookApi);
+        }
+    }
+}
+
+
---
+++ Program_Sin_Iterator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string FbName { get; set; }
+        public string FbUrl { get; set; }
+
+        public User(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public User(string fbName, string fbUrl, bool isFacebookUser = true)
+        {
+            if (isFacebookUser)
+            {
+                FbName = fbName;
+                FbUrl = fbUrl;
+            }
+        }
+
+        public string GetInfo(bool isFacebookUser = false)
+        {
+            if (isFacebookUser)
+            {
+                return $"Name: {FbName}, URL: {FbUrl}";
+            }
+            else
+            {
+                return $"Name: {Name}, Email: {Email}";
+            }
+        }
+    }
+
+    public class ShowUsersPanel
+    {
+        public void ShowUsers(List<User> users)
+        {
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.GetInfo());
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var showUsersPanel = new ShowUsersPanel();
+
+            Console.WriteLine("Google Users:");
+            var googleUsers = new List<User>
+            {
+                new User("roberto", "r@gmail.com"),
+                new User("luis", "l@gmail.com"),
+                new User("ana", "a@gmail.com")
+            };
+            showUsersPanel.ShowUsers(googleUsers);
+
+            Console.WriteLine("Facebook Users:");
+            var facebookUsers = new List<User>
+            {
+                new User("juan", "link1", true),
+                new User("carlos", "link2", true)
+            };
+            showUsersPanel.ShowUsers(facebookUsers);
+        }
+    }
+}
